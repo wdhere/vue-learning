611dc9919376368e4416990254b72fd9e91b080d
wdhere <wdhere@163.com>
2023-01-03 14:21:47 +0800

    feat: Component, Computed, Methods

---
+++ demos/upvote/app/main.js
@@ -0,0 +1,59 @@
+const submissionComponent = {
+  template: `<div style="display: flex; width: 100%">
+    <figure class="media-left">
+      <img :src="submission.submissionImage" alt="yellow" class="image is-64x64">
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>
+            <a :href="submission.url" class="has-text-info">{{ submission.title }}</a>
+            <span class="tag is-small">#{{ submission.id }}</span>
+          </strong>
+          <br>
+          {{ submission.description }}
+          <br>
+          <small class="is-size-7">
+            Submitted by:
+            <img :src="submission.avatar" alt="daniel" class="image is-24x24">
+          </small>
+        </p>
+      </div>
+    </div>
+    <div class="media-right">
+      <span class="icon is-small" @click="upvote(submission.id)">
+        <i class="fa fa-chevron-up"></i>
+        <strong class="has-text-info">{{ submission.votes }}</strong>
+      </span>
+    </div>
+  </div>`,
+  props: ["submission", "submissions"],
+  methods: {
+    upvote(submissionId) {
+      const submission = this.submissions.find(
+        (submission) => submission.id === submissionId
+      );
+      submission.votes++;
+    },
+  },
+};
+
+const upvoteApp = {
+  data() {
+    return {
+      submissions: Seed.submissions,
+    };
+  },
+  computed: {
+    sortedSubmissions() {
+      return this.submissions.sort((a, b) => {
+        return b.votes - a.votes;
+      });
+    },
+  },
+  components: {
+    "submission-component": submissionComponent,
+  },
+};
+
+Vue.createApp(upvoteApp).mount("#app");
