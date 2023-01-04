ea8cc5f96c3cc27a4fad2d41516bd580b86e1d25
wdhere <wdhere@163.com>
2023-01-04 16:52:06 +0800

    feat: vuex deploy product data

--- shopping_cart/src/app/components/product/ProductList.vue
+++ shopping_cart/src/app/components/product/ProductList.vue
@@ -4,22 +4,12 @@
       <i class="fa fa-2x fa-user-circle"></i>
     </div>
     <div class="product-list">
-      <div class="product-list--item">
-        <div>
-          <h2 class="has-text-weight-bold">The Fullstack Hoodie
-            <span class="tag 
-                is-primary
-                is-pulled-right
-                has-text-white">
-              Add to Cart
-            </span>
-          </h2>
-          <p>Lightweight, breathable hoodie with the Fullstack Crest.
-          Guaranteed to keep you looking fresh while warm.</p>
-          <span class="has-text-primary has-text-weight-bold">
-            <i class="fa fa-usd"></i> 19.99
-          </span>
-        </div>
+      <div
+        class="product-list--item"
+        v-for="productItem in productItems"
+        :key="productItem.id"
+      >
+        <ProductListItem :productItem="productItem" />
       </div>
     </div>
     <div class="product-count has-text-right">
@@ -29,9 +19,21 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import ProductListItem from "./ProductListItem";
+
 export default {
-  name: 'ProductList',
-}
+  name: "ProductList",
+  computed: {
+    ...mapGetters(["productItems"]),
+  },
+  created() {
+    this.$store.dispatch("getProductItems");
+  },
+  components: {
+    ProductListItem,
+  },
+};
 </script>
 
 <style scoped>
@@ -40,7 +42,7 @@ export default {
 }
 
 .products--header {
-  border-bottom: 1px solid #E8E8E8;
+  border-bottom: 1px solid #e8e8e8;
   padding-bottom: 15px;
 }
 
--- shopping_cart/src/main.js
+++ shopping_cart/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./app/App.vue";
+import store from "./app/store";
 
-createApp(App).mount("#app");
+createApp(App)
+  .use(store)
+  .mount("#app");
