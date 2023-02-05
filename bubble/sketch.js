8f3c8caf06539a6c676603b95dfe47b98622868f
wdhere <wdhere@163.com>
2023-02-06 00:52:06 +0800

    chore: update

--- bubble/sketch.js
+++ bubble/sketch.js
@@ -1,17 +1,11 @@
 let sketch = function (p) {
   let ox = p.random(10000);
   let oy = p.random(10000);
-  const width = window.innerWidth - 10;
-  const height = window.innerHeight - 10;
-  // alert("width: " + window.innerWidth + " height: " + window.innerHeight);
-  const radiusBase = width > height ? width * 0.15 : height * 0.15;
-  const part = radiusBase / 3;
-  const speed = radiusBase * 0.003;
-  let radius = radiusBase;
+  let radius = 200;
   let acc = true;
 
   p.setup = function () {
-    p.createCanvas(width, height);
+    p.createCanvas(800, 800);
     p.strokeWeight(2);
     p.stroke(255);
     p.smooth();
@@ -33,28 +27,27 @@ let sketch = function (p) {
     p.beginShape();
     for (let angle = 0; angle < 360; angle += 3) {
       let radian = p.radians(angle);
-      let radiusN =
-        radius + p.map(getNoise(radian, 0.25), 0, 1, -0.8 * part, 0.8 * part);
+      let radiusN = radius + p.map(getNoise(radian, 0.25), 0, 1, -60, 60);
       p.vertex(radiusN * p.cos(radian), radiusN * p.sin(radian));
     }
     p.endShape(p.CLOSE);
   }
 
   function animateR() {
-    if (radius >= radiusBase + part) {
-      radius = radiusBase + part;
+    if (radius >= 270) {
+      radius = 270;
       acc = false;
     }
 
-    if (radius <= radiusBase - part) {
-      radius = radiusBase - part;
+    if (radius <= 130) {
+      radius = 130;
       acc = true;
     }
 
     if (acc) {
-      radius += speed;
+      radius += 0.5;
     } else {
-      radius -= speed;
+      radius -= 0.5;
     }
   }
 
