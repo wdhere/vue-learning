71c0bb7d3f233fdd783478c2e783ca3c1fbfe595
wdhere <wdhere@163.com>
2023-02-05 22:07:59 +0800

    chore: update

--- bubble/sketch.js
+++ bubble/sketch.js
@@ -12,6 +12,7 @@ let sketch = function (p) {
   p.setup = function () {
     p.createCanvas(width, height);
     p.pixelDensity(1);
+    p.frameRate(30);
     p.strokeWeight(2);
     p.stroke(255);
     p.smooth();
